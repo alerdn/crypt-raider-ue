146f252ab0dda8d3764a41970eda8b68165a7f89
Alexandre Nascimento <alexandre.nascimento.ifms@gmail.com>
2024-02-26 19:03:13 -0400

    feat: reconhecendo actor aceitável no trigger da porta secreta

--- Source/CryptRaider/TriggerComponent.cpp
+++ Source/CryptRaider/TriggerComponent.cpp
@@ -1,9 +1,35 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #include "TriggerComponent.h"
 
-// Called when the game starts
+UTriggerComponent::UTriggerComponent()
+{
+    PrimaryComponentTick.bCanEverTick = true;
+}
+
 void UTriggerComponent::BeginPlay()
 {
     Super::BeginPlay();
+}
+
+void UTriggerComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+{
+    Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+    GetAcceptableActor();
+}
+
+AActor *UTriggerComponent::GetAcceptableActor() const
+{
+    TArray<AActor *> Actors;
+    GetOverlappingActors(Actors);
+
+    for (AActor *Actor : Actors)
+    {
+        if (Actor->ActorHasTag(AcceptableActorTag))
+        {
+            UE_LOG(LogTemp, Display, TEXT("Unlocking"));
+            return Actor;
+        }
+    }
+
+    return nullptr;
 }
